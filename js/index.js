4da119496c55c9de3f9a59cc4b15eb1c966f0eaa
Pavel Kuzmin <pavel619games@yandex.ru>
2022-01-21 15:58:22 +0300

    добавил секцию dishes

--- js/index.js
+++ js/index.js
@@ -18,3 +18,18 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active')
 }
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
